42999d08ef95b37256182ba9c0d23260a8871e24
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-12-27 20:49:46 +0800

    chore(release): 0.1.0

--- auto-android/build.gradle.kts
+++ auto-android/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "top.anagke"
-version = "1.0.0"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -18,30 +18,21 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("stdlib-jdk8"))
 
+    // Log Frameworks
+    implementation("ch.qos.logback:logback-core:1.3.0-alpha5")
     implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
-    implementation("io.github.microutils:kotlin-logging:2.1.16")
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
 
+    // OpenCV
     implementation("org.openpnp:opencv:4.5.1-2")
     implementation("top.anagke:kio:1.1.0")
 
-    // https://mvnrepository.com/artifact/org.jsoup/jsoup
-    implementation("org.jsoup:jsoup:1.14.3")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.1")
-    implementation("com.charleskorn.kaml:kaml:0.38.0")
-    implementation("com.google.code.gson:gson:2.8.9")
-
-
+    // Utilities
     implementation("com.github.albfernandez:juniversalchardet:2.4.0")
     implementation("info.debatty:java-string-similarity:2.0.0")
 
-    // Config Parsing
-    implementation("com.sksamuel.hoplite:hoplite:1.0.3")
-    implementation("com.sksamuel.hoplite:hoplite-toml:1.4.16")
-
-
-    // CLI Argument Parsing
-    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+    //Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.1")
 }
 
 java {
