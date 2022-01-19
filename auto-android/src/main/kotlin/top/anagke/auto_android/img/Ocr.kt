5809b281bf755ed2574da1e7680cbcb9d9824c87
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-01-19 14:50:56 +0800

    feat: 更新 tesseract 和 Ocr.kt 的接口

--- auto-android/src/main/kotlin/top/anagke/auto_android/img/Ocr.kt
+++ auto-android/src/main/kotlin/top/anagke/auto_android/img/Ocr.kt
@@ -1,5 +1,6 @@
 package top.anagke.auto_android.img
 
+import info.debatty.java.stringsimilarity.JaroWinkler
 import info.debatty.java.stringsimilarity.NormalizedLevenshtein
 import mu.KotlinLogging
 import top.anagke.auto_android.BinResources
@@ -38,4 +39,25 @@ fun Img.ocr(possibleWords: List<String> = emptyList()): String {
     val result = possibleWords.maxByOrNull { NormalizedLevenshtein().similarity(it, ocr) } ?: ocr
     logger.debug { "Ocr RESULT $this: '$result'" }
     return result
+}
+
+fun ocrWord(img: Img, words: List<String> = emptyList()): String {
+    BinResources.init()
+
+    logger.debug { "OCR using pre-built tesseract v5.0.1.20220118" }
+    val tesseractExec = "bin/tesseract/tesseract.exe"
+
+    // Page Segmentation Modes:
+    // ...
+    // 7    Treat the image as a single text line.
+    // 8    Treat the image as a single word.
+    // ...
+    val proc = openProc(tesseractExec, "stdin", "stdout", "-l", "chi_sim+eng", "--psm", "8")
+    proc.outputStream.write(Img.encode(img))
+
+    val stdout = proc.readText().stdout.replace(Regex("\\s"), "")
+    val bestWord = words.minByOrNull { JaroWinkler().distance(it, stdout) } ?: stdout
+
+    logger.debug { "OCR result: best word '$bestWord', stdout '$stdout'" }
+    return bestWord
 }
