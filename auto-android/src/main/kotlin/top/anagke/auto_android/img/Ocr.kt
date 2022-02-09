a9869f5119ea31bc99ff6bcf224ddb4242167f66
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-02-09 18:33:26 +0800

    refactor: remove redundant codes

--- auto-android/src/main/kotlin/top/anagke/auto_android/img/Ocr.kt
+++ auto-android/src/main/kotlin/top/anagke/auto_android/img/Ocr.kt
@@ -1,45 +1,12 @@
 package top.anagke.auto_android.img
 
 import info.debatty.java.stringsimilarity.JaroWinkler
-import info.debatty.java.stringsimilarity.NormalizedLevenshtein
 import mu.KotlinLogging
 import top.anagke.auto_android.native.openProc
 import top.anagke.auto_android.native.waitText
 import top.anagke.auto_android.util.BinResources
-import top.anagke.kio.util.TempFiles
-import top.anagke.kio.util.useTempFile
-import java.nio.file.Files
 
 private val logger = KotlinLogging.logger { }
-
-fun ocrTesseract(img: Img): String {
-    return img.ocr()
-}
-
-fun Img.ocr(possibleWords: List<String> = emptyList()): String {
-    BinResources.init()
-    logger.debug { "OCRing using prebuilt-tesseract-x86 $this" }
-
-    var ocr = ""
-    TempFiles.useTempFile(TempFiles.LOCAL_TEMP_DIR) {
-        Files.write(it, Img.encode(this))
-
-        val proc = openProc(
-            "bin/tesseract/tesseract.exe",
-            "$it", "-",
-            "-l", "chi_sim",
-            "--psm", "12",
-            "--dpi", "300",
-        )
-        val output = proc.waitText().stdout
-
-        ocr = output.trim().replace(Regex("\\s"), "")
-        logger.debug { "Ocr RAW $this: '$ocr'" }
-    }
-    val result = possibleWords.maxByOrNull { NormalizedLevenshtein().similarity(it, ocr) } ?: ocr
-    logger.debug { "Ocr RESULT $this: '$result'" }
-    return result
-}
 
 fun ocrWord(img: Img, words: List<String> = emptyList()): String {
     BinResources.init()
