8ca12d14d9833f08627c432288206021ffccb817
Flandia Yingman <Flandia_YingM@hotmail.com>
2022-02-09 20:23:46 +0800

    feat: table selector

--- auto-android/src/main/kotlin/top/anagke/auto_android/device/Ops.kt
+++ auto-android/src/main/kotlin/top/anagke/auto_android/device/Ops.kt
@@ -2,6 +2,8 @@
 
 package top.anagke.auto_android.device
 
+import top.anagke.auto_android.device.TableSelector.ConstraintType.HORIZONTAL
+import top.anagke.auto_android.device.TableSelector.ConstraintType.VERTICAL
 import top.anagke.auto_android.img.Img
 import top.anagke.auto_android.img.Tmpl
 import top.anagke.auto_android.util.*
@@ -170,4 +172,81 @@ fun Device.tapdListItem(itemIndex: Int, itemLength: Int, x: Int, y: Int, dragDy:
         }
         tapd(x, y + ((itemLength - 1) * tapDy)).nap()
     }
+}
+
+data class TableSelector(
+    val origin: Pos,
+    val finale: Pos,
+    val itemInterval: Size,
+    val dragInterval: Size,
+    val viewWidth: Int,
+    val viewHeight: Int,
+    val tableWidth: Int,
+    val tableHeight: Int,
+    val tableConstraintType: ConstraintType? = null,
+    var viewX: Int = 0,
+    var viewY: Int = 0,
+) {
+
+    enum class ConstraintType {
+        HORIZONTAL, VERTICAL,
+    }
+
+    fun fromSeqNum(itemSeqNum: Int): Pos {
+        tableConstraintType!!
+
+        val tableConstraint = when (tableConstraintType) {
+            HORIZONTAL -> tableWidth
+            VERTICAL -> tableHeight
+        }
+        val seq = itemSeqNum % tableConstraint
+        val carry = itemSeqNum / tableConstraint
+        return when (tableConstraintType) {
+            HORIZONTAL -> Pos(seq, carry)
+            VERTICAL -> Pos(carry, seq)
+        }
+    }
+
+    fun Device.tapItem(item: Pos) {
+        var itemX = item.x
+        var itemY = item.y
+        if (itemX >= viewWidth) {
+            val swipeCount = itemX - (viewWidth - 1) - viewX
+            viewX += swipeCount
+            repeat(swipeCount) {
+                dragv(origin.x, origin.y, -dragInterval.width, 0)
+            }
+            if (viewX + viewWidth == tableWidth) {
+                swipev(origin.x, origin.y, -dragInterval.width, 0, 1.0)
+            }
+            itemX = viewWidth - 1
+        }
+        if (itemY >= viewHeight) {
+            val swipeCount = itemY - (viewHeight - 1) - viewY
+            viewY += swipeCount
+            repeat(swipeCount) {
+                dragv(origin.x, origin.y, 0, -dragInterval.height)
+            }
+            if (viewY + viewHeight == tableHeight) {
+                swipev(origin.x, origin.y, 0, -dragInterval.height, 1.0)
+            }
+            itemY = viewHeight - 1
+        }
+        val xOffset = if (viewX + viewWidth == tableWidth && itemX == viewWidth - 1) {
+            finale.x
+        } else {
+            origin.x + itemX * itemInterval.width
+        }
+        val yOffset = if (viewY + viewHeight == tableHeight && itemY == viewHeight - 1) {
+            finale.y
+        } else {
+            origin.y + itemY * itemInterval.height
+        }
+        tap(xOffset, yOffset)
+    }
+
+    fun Device.resetTable() {
+        swipev(origin.x, origin.y, dragInterval.width * tableWidth, dragInterval.height * tableHeight, 3.0)
+    }
+
 }
--- auto-android/src/main/kotlin/top/anagke/auto_android/util/Pos.kt
+++ auto-android/src/main/kotlin/top/anagke/auto_android/util/Pos.kt
@@ -1,5 +1,21 @@
 package top.anagke.auto_android.util
 
-data class Pos(val x: Int, val y: Int) {
+data class Pos(val x: Int, val y: Int) : Comparable<Pos> {
     override fun toString(): String = "($x, $y)"
+
+    override fun compareTo(other: Pos): Int {
+        return Comparator
+            .comparingInt<Pos> { it.y }
+            .thenComparingInt { it.x }
+            .compare(this, other)
+    }
+
+    operator fun plus(that: Pos): Pos {
+        return Pos(this.x + that.x, this.y + that.y)
+    }
+
+    operator fun minus(that: Pos): Pos {
+        return Pos(this.x - that.x, this.y - that.y)
+    }
+
 }
