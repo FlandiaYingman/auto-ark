3c9e638ed9f535a260fc6f20f2b4fad2bc705f69
Flandia Yingman <flandia_yingm@hotmail.com>
2023-02-11 23:04:19 +0800

    build: update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    kotlin("jvm") version "1.8.20-Beta"
+    kotlin("plugin.serialization") version "1.8.20-Beta"
 
+    id("com.github.ben-manes.versions") version "0.45.0"
     application
 }
 
@@ -20,19 +21,19 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("org.tinylog:tinylog-impl:2.5.0")
-    implementation("org.tinylog:tinylog-api-kotlin:2.5.0")
-    implementation("org.tinylog:slf4j-tinylog:2.5.0")
+    implementation("org.tinylog:tinylog-impl:2.6.0")
+    implementation("org.tinylog:tinylog-api-kotlin:2.6.0")
+    implementation("org.tinylog:slf4j-tinylog:2.6.0")
 
-    implementation("org.openpnp:opencv:4.5.5-1")
+    implementation("org.openpnp:opencv:4.6.0-0")
     implementation("top.anagke:kio:1.1.0")
 
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.15.3")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.0")
-    implementation("com.charleskorn.kaml:kaml:0.47.0")
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0-RC")
+    implementation("com.charleskorn.kaml:kaml:0.51.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
 
     implementation("com.github.albfernandez:juniversalchardet:2.4.0")
@@ -40,7 +41,7 @@ dependencies {
 
     // Config Parsing
     implementation("com.sksamuel.hoplite:hoplite:1.0.3")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.6.3")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.7.1")
 
     // CLI Argument Parsing
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
@@ -53,7 +54,7 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.wrapper {
-    gradleVersion = "7.5"
+    gradleVersion = "7.6"
 }
 
 application {
