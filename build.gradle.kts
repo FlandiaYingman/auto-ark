eaf0094b0190cab3e0cbff029c47d3e4b8a5e8b7
Flandia Yingman <flandia_yingm@hotmail.com>
2023-07-05 06:10:25 +0800

    build: 升级依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.20-Beta"
-    kotlin("plugin.serialization") version "1.8.20-Beta"
+    kotlin("jvm") version "1.9.0-RC"
+    kotlin("plugin.serialization") version "1.9.0-RC"
 
-    id("com.github.ben-manes.versions") version "0.45.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
     application
 }
 
 group = "top.anagke.auto_ark"
-version = "0.2.0"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -21,18 +21,18 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("org.tinylog:tinylog-impl:2.6.0")
-    implementation("org.tinylog:tinylog-api-kotlin:2.6.0")
-    implementation("org.tinylog:slf4j-tinylog:2.6.0")
+    implementation("org.tinylog:tinylog-impl:2.6.2")
+    implementation("org.tinylog:tinylog-api-kotlin:2.6.2")
+    implementation("org.tinylog:slf4j-tinylog:2.6.2")
 
-    implementation("org.openpnp:opencv:4.6.0-0")
+    implementation("org.openpnp:opencv:4.7.0-0")
     implementation("top.anagke:kio:1.1.0")
 
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
-    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("org.jsoup:jsoup:1.16.1")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0-RC")
-    implementation("com.charleskorn.kaml:kaml:0.51.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+    implementation("com.charleskorn.kaml:kaml:0.54.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
 
@@ -41,7 +41,7 @@ dependencies {
 
     // Config Parsing
     implementation("com.sksamuel.hoplite:hoplite:1.0.3")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.7.1")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.8.0.RC1")
 
     // CLI Argument Parsing
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
@@ -51,10 +51,6 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "17"
     }
-}
-
-tasks.wrapper {
-    gradleVersion = "7.6"
 }
 
 application {
