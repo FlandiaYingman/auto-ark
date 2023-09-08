2b2fff6c457b2396cf487b15cb12bb12fc7c865a
Flandia Yingman <flandia_yingm@hotmail.com>
2023-09-09 01:28:12 +0800

    feat: 支持活动“不义之财”

--- src/main/kotlin/top/anagke/auto_ark/event_scanner/ArtEventScanner.kt
+++ src/main/kotlin/top/anagke/auto_ark/event_scanner/ArtEventScanner.kt
@@ -4,12 +4,16 @@ import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.encodeToString
 import top.anagke.auto_android.device.nap
 import top.anagke.auto_android.device.sleep
+import top.anagke.auto_android.img.DetResult
 import top.anagke.auto_android.img.Img
 import top.anagke.auto_android.img.Pos
 import top.anagke.auto_android.img.det
+import top.anagke.auto_android.util.Rect
 import top.anagke.auto_ark.App
 import top.anagke.auto_ark.ArkModule
 import top.anagke.auto_ark.AutoArk
+import top.anagke.auto_ark.operate.OperatePoses.终端_副活动
+import top.anagke.auto_ark.operate.OperatePoses.终端_活动
 import top.anagke.auto_ark.operate.Root
 import top.anagke.auto_ark.operate.Stage
 import top.anagke.auto_ark.resetInterface
@@ -28,16 +32,19 @@ class ArtEventScanner(
 
     override val name: String = "活动扫描模块"
     override fun run(): Unit = device.run {
-        val targetEventName = "火山旅梦"
-        val targetZoneName = "温泉假日"
-        val targetStagePrefix = "SL-"
+        val targetEventName = "不义之财"
+        val targetZoneName = "苍白平台"
+        val targetStagePrefix = "CV-"
 
         resetInterface()
 
         val root: Root = linkedMapOf(targetEventName to linkedMapOf(targetZoneName to linkedMapOf()))
         val possibleEvents = det(cap())
             .filter { targetEventName in it.text }
-        for (possibleEvent in possibleEvents) {
+        for (possibleEvent in possibleEvents + listOf(
+            DetResult(Rect(终端_活动, 终端_活动), "", 1.0),
+            DetResult(Rect(终端_副活动, 终端_副活动), "", 1.0)
+        )) {
             tap(possibleEvent.box.center(), desc = "Possible Event").sleep()
             val possibleZones = det(cap())
                 .filter { targetZoneName in it.text }
