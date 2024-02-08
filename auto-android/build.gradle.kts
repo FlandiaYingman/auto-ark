843fcce2fb422da523bd7b5cbb6fd471a59ab0fe
Flandia Yingman <me@flandia.dev>
2024-02-09 02:10:08 +0800

    build: 升级 Gradle 及 Dependencies

--- auto-android/build.gradle.kts
+++ auto-android/build.gradle.kts
@@ -19,22 +19,22 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     // Log Frameworks
-    implementation("org.tinylog:tinylog-impl:2.6.2")
-    implementation("org.tinylog:tinylog-api-kotlin:2.6.2")
-    implementation("org.tinylog:slf4j-tinylog:2.6.2")
+    implementation("org.tinylog:tinylog-impl:2.7.0")
+    implementation("org.tinylog:tinylog-api-kotlin:2.7.0")
+    implementation("org.tinylog:slf4j-tinylog:2.7.0")
 
     // OpenCV
-    implementation("org.bytedeco:opencv-platform:4.7.0-1.5.9")
+    implementation("org.bytedeco:opencv-platform:4.9.0-1.5.10")
     implementation("top.anagke:kio:1.1.0")
 
     // Utilities
     implementation("info.debatty:java-string-similarity:2.0.0")
     implementation("org.reflections:reflections:0.10.2")
 
-    implementation("net.java.dev.jna:jna:5.13.0")
-    implementation("net.java.dev.jna:jna-platform:5.13.0")
+    implementation("net.java.dev.jna:jna:5.14.0")
+    implementation("net.java.dev.jna:jna-platform:5.14.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.2")
 }
 
 tasks.withType<KotlinCompile> {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0-RC"
-    kotlin("plugin.serialization") version "1.9.0-RC"
+    kotlin("jvm") version "2.0.0-Beta3"
+    kotlin("plugin.serialization") version "2.0.0-Beta3"
 
-    id("org.bytedeco.gradle-javacpp-platform") version "1.5.9"
-    id("com.github.ben-manes.versions") version "0.47.0"
+    id("org.bytedeco.gradle-javacpp-platform") version "1.5.10"
+    id("com.github.ben-manes.versions") version "0.51.0"
     application
 }
 
@@ -21,19 +21,19 @@ dependencies {
 
     implementation(kotlin("stdlib"))
 
-    implementation("org.tinylog:tinylog-impl:2.6.2")
-    implementation("org.tinylog:tinylog-api-kotlin:2.6.2")
-    implementation("org.tinylog:slf4j-tinylog:2.6.2")
+    implementation("org.tinylog:tinylog-impl:2.7.0")
+    implementation("org.tinylog:tinylog-api-kotlin:2.7.0")
+    implementation("org.tinylog:slf4j-tinylog:2.7.0")
 
     // OpenCV
-    implementation("org.bytedeco:opencv-platform:4.7.0-1.5.9")
+    implementation("org.bytedeco:opencv-platform:4.9.0-1.5.10")
     implementation("top.anagke:kio:1.1.0")
 
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
-    implementation("org.jsoup:jsoup:1.16.1")
+    implementation("org.jsoup:jsoup:1.17.2")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
-    implementation("com.charleskorn.kaml:kaml:0.54.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.2")
+    implementation("com.charleskorn.kaml:kaml:0.57.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
 
@@ -41,7 +41,7 @@ dependencies {
 
     // Config Parsing
     implementation("com.sksamuel.hoplite:hoplite:1.0.3")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.8.0.RC1")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.8.0.RC3")
 }
 
 tasks.withType<KotlinCompile> {
